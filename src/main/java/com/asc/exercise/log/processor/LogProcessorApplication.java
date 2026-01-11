bae51d2b05d4ceda45aec17de450e6cfdf2282da
chughpuneet <chughpuneet@gmail.com>
2026-01-12 03:54:27 +1100

    Validate input

--- src/main/java/com/asc/exercise/log/processor/LogProcessorApplication.java
+++ src/main/java/com/asc/exercise/log/processor/LogProcessorApplication.java
@@ -5,27 +5,43 @@ import com.asc.exercise.log.processor.input.TextFileInputProcessor;
 import com.asc.exercise.log.processor.output.ConsoleOutputWriter;
 import com.asc.exercise.log.processor.output.JsonFileOutputWriter;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class LogProcessorApplication {
 
     public static void main(String[] args) {
-        LogProcessor processor = null;
         try (Scanner scanner = new Scanner(System.in)) {
 
             System.out.print("Enter input text file path: ");
-            String inputFilePath = scanner.nextLine();
+            Path inputPath = Path.of(scanner.nextLine());
+
+            if (Files.notExists(inputPath) || !Files.isRegularFile(inputPath)) {
+                System.err.println("Invalid input file path: " + inputPath);
+                return;
+            }
 
             System.out.print("Enter output JSON file path: ");
-            String outputFilePath = scanner.nextLine();
+            String outputPathStr = scanner.nextLine();
 
-            processor = new LogProcessor(new TextFileInputProcessor(inputFilePath, new TextAnalyzer()),
+            if (null == outputPathStr || outputPathStr.isEmpty()) {
+                System.err.println("Invalid output file path: " + outputPathStr);
+                return;
+            }
+            Path outputPath = Path.of(outputPathStr);
+
+            LogProcessor processor = new LogProcessor(
+                    new TextFileInputProcessor(inputPath.toString(), new TextAnalyzer()),
                     new ConsoleOutputWriter(),
-                    new JsonFileOutputWriter(outputFilePath));
+                    new JsonFileOutputWriter(outputPath.toString())
+            );
+
             processor.process();
+            System.out.println("Processing completed successfully.");
 
         } catch (Exception e) {
-            System.err.println("Error during processing: " + e.getMessage());
+            System.err.println("Unexpected error: " + e.getMessage());
         }
     }
 }
